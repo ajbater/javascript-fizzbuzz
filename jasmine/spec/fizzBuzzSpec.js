d683bea8e4094144c8085a209c63705ca4dc365d
ajbater <amanda22@hotmail.co.uk>
2016-12-06 12:51:57 +0000

    divisible by 15

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -8,22 +8,32 @@ describe('FizzBuzz', function() {
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
 
-    it('divisible by 5', function(){
+    it('divisible by 5', function() {
       fizzbuzz = new FizzBuzz();
       expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+    it('divisible by 15', function() {
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
     });
   });
 
   describe('knows when a number is NOT', function() {
 
-    it(('divisble by 3'), function() {
+    it('divisble by 3', function() {
       fizzbuzz = new FizzBuzz();
       expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
     });
 
-    it(('divisible by 5'), function(){
+    it('divisible by 5', function(){
       fizzbuzz = new FizzBuzz();
       expect(fizzbuzz.isDivisibleByFive(6)).toBe(false);
+    });
+
+    it('divisible by 15', function() {
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByFifteen(16)).toBe(false);
     });
   });
 
