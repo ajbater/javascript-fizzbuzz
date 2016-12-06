53775edc07c426b764fddc623f74ba8c97bf6f19
DarkChocLatte <darkchoclatte@gmail.com>
2016-12-06 12:43:00 +0000

    buzz

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -7,6 +7,11 @@ describe('FizzBuzz', function() {
       fizzbuzz = new FizzBuzz();
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
+
+    it('divisible by 5', function(){
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
   });
 
   describe('knows when a number is NOT', function() {
@@ -15,6 +20,12 @@ describe('FizzBuzz', function() {
       fizzbuzz = new FizzBuzz();
       expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
     });
+
+    it(('divisible by 5'), function(){
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByFive(6)).toBe(false);
+    });
   });
+
 
 });
